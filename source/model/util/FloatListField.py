8d07dc15fc98aa5590696273fae7fdc79a6eeaf5
Nils Prommersberger <nils.prommersberger@icloud.com>
2024-06-13 20:33:20 +0200

    Update model

--- source/model/util/FloatListField.py
+++ source/model/util/FloatListField.py
@@ -3,4 +3,25 @@ from django.db import models
 
 # List of floats to store the results of the simulation in the database
 class FloatListField(models.Field):  # Maybe it needs something else than models.Field
-    pass  # TODO: implement this class
+    # The maximum length of the list
+    _max_length: int
+
+    # Get the floats of this list
+    def get_all(self) -> [float]:
+        return None  # TODO: implement this function
+
+    # Get the float at the given index
+    def get(self, index: int) -> float:
+        return None  # TODO: implement this function
+
+    # Set the float at the given index
+    def set(self, index: int, value: float):
+        pass  # TODO: implement this function
+
+    # Get the maximum length of the list
+    def get_max_length(self) -> int:
+        return self._max_length
+
+    # Get the average of all floats in the list
+    def get_average(self) -> float:
+        return None  # TODO: implement this function
