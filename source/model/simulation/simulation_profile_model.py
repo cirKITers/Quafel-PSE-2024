c93b55b8b4523ac4620e2dc270fd532522b71ecc
Nils Prommersberger <nils.prommersberger@icloud.com>
2024-06-15 10:10:54 +0200

    code style and setup poetry

--- source/model/simulation/simulation_profile_model.py
+++ source/model/simulation/simulation_profile_model.py
@@ -1,10 +1,17 @@
-from django.db import models
+"""
+This file contains the SimulationProfileModel class, 
+which represents an api_simulation program and version on a hardware_profile.
+"""
 
+from django.db import models
 from source.model.hardware_profile.hardware_profile_model import HardwareProfileModel
 
 
-# This class represents an api_simulation program and version on a hardware_profile
 class SimulationProfileModel(models.Model):
+    """
+    This class represents an api_simulation program and version on a hardware_profile.
+    """
+
     # The name of the api_simulation program
     _name = models.CharField(max_length=100)
 
--- source/model/util/float_list_field.py
+++ source/model/util/float_list_field.py
@@ -1,27 +1,50 @@
+"""
+This file contains the FloatListField class, which is a custom field for Django models.
+"""
 from django.db import models
 
 
-# List of floats to store the results of the api_simulation in the database
 class FloatListField(models.Field):  # Maybe it needs something else than models.Field
+    """
+    This class represents a list of floats as a custom field for Django models.
+    """
+
     # The maximum length of the list
     _max_length: int
 
-    # Get the floats of this list
     def get_all(self) -> [float]:
-        return None  # TODO: implement this function
+        """
+        Get all floats in the list.
+        :return: List of float containing all floats in the list.
+        """
+        # TODO: implement this function
 
-    # Get the float at the given index
     def get(self, index: int) -> float:
-        return 0  # TODO: implement this function
+        """
+        Get the float at the given index.
+        :param index: The index of the float.
+        :return: The float at the given index.
+        """
+        # TODO: implement this function
 
-    # Set the float at the given index
     def set(self, index: int, value: float):
-        pass  # TODO: implement this function
+        """
+        Set the float at the given index to the given value.
+        :param index: Index of the float.
+        :param value: Value to set the float to.
+        """
+        # TODO: implement this function
 
-    # Get the maximum length of the list
     def get_max_length(self) -> int:
+        """
+        Get the maximum length of the list.
+        :return: The maximum length of the list.
+        """
         return self._max_length
 
-    # Get the average of all floats in the list
     def get_average(self) -> float:
+        """
+        Get the average of all floats in the list.
+        :return: The average of all floats in the list.
+        """
         return 0.0  # TODO: implement this function
