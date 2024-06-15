4a076a044b546bc06137314d08c61b5e647b724d
Nils Prommersberger <nils.prommersberger@icloud.com>
2024-06-15 10:15:45 +0200

    test codestyle check

---
+++ source/api_account/test.py
@@ -0,0 +1,13 @@
+"""
+This should work as a test file for the api_account app.
+"""
+
+class noCamelCase:
+    """
+    This class should not be detected as a test class.
+    """
+    def test_Method(self):
+        """
+        This method should not be detected as a test method.
+        """
+        pass
