ca0d4fadba9188df695d6d5a312795235ce60515
Julius Haag <haag.julius@outlook.de>
2024-06-19 23:36:15 +0200

    Tested graph rendering

--- quafelweb/admin/auth/authenticate.py
+++ quafelweb/admin/auth/authenticate.py
@@ -1,7 +1,10 @@
 import json
+from django.http import HttpRequest
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from authlib.integrations.django_client import OAuth
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.models import User
 
 CONF_URL = "https://oidc.scc.kit.edu/auth/realms/kit/.well-known/openid-configuration"
 oauth = OAuth()
@@ -12,3 +15,14 @@ oauth.register(
         'scope': 'openid email profile'
     }
 )
+
+
+class MyBackend(BaseBackend):
+  def authenticate(self, request : HttpRequest):
+    return oauth.kitopenid.authorize_redirect(request, request.get_raw_uri())
+  
+  def get_user(self, user_id):
+    try:
+      return User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+      return None
--- quafelweb/public/simulation_visualization/views.py
+++ quafelweb/public/simulation_visualization/views.py
@@ -5,6 +5,18 @@
 
 from django.shortcuts import render
 
+import plotly.express as px
+import pandas as pd
 
-def index(req):
-  return render(req, "index.html")
+def index(request):
+    # Generate the plot
+    df = pd.DataFrame({
+        "Qbits": list(range(16)),
+        "Duration": list(1000 * (i ** 2) for i in range(16))
+    })
+    fig = px.plot(df, x="Qbits", y="Duration")
+    graph_div = fig.to_html(full_html=False)
+
+    # Pass the plot to the HTML template
+    context = {'graph_div': graph_div}
+    return render(request, 'index.html', context)
