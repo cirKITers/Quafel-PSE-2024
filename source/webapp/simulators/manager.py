d96bb7af280ce4a0ad235bb5198f07f4f247cbb6
Paul Schillinger <uystq@student.kit.edu>
2024-06-20 21:55:41 +0200

    slurm

--- source/webapp/simulators/manager.py
+++ source/webapp/simulators/manager.py
@@ -7,25 +7,25 @@ from source.api_node.i_node_run import INodeRun
 # Maybe more methods?
 
 
-def run(username:str, host:str, port:str, password:str, totp: str,
+def run(username:str, host:str, port:str, password:str, totp: str, slurm: str,
         min_qubits: int, max_qubits: int, qubits_increment: int, qubits_type: str, 
         min_depth: int, max_depth: int, depth_increment: int, depth_type: str, 
         min_shots: int, max_shots: int, shots_increment: int, shots_type: str,
         quantum_framework: list[str], evaluations: int):
     """
-    Connect to node through ssh with username, host, port, password
+    Connect to node through ssh with username, host, port, password, 
     if correct QUAFEL version is not set up, set it up
-    Run QUAFEL with given parameters
+    Run QUAFEL with given parameters through SLURM
     """
 
 
-def get_output(username:str, host:str, port:str, password:str, totp: str):
+def get_output(username:str, host:str, port:str, password:str, totp: str, slurm: str):
     """
     Get the output of the node-run.
     """
 
 
-def update(username:str, host:str, port:str, password:str, totp: str):
+def update(username:str, host:str, port:str, password:str, totp: str, slurm: str):
     """
     Look if the output is finished and write it to the database. 
     This can be must be done by a separate thread.
