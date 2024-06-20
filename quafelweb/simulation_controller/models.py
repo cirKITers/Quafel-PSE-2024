798ea6d46b7a67632c29a64a2d58c446a9822740
Julius Haag <haag.julius@outlook.de>
2024-06-20 22:22:57 +0200

    improved simulationview

--- quafelweb/simulation_controller/models.py
+++ quafelweb/simulation_controller/models.py
@@ -1,6 +1,8 @@
 # Create your models here.
 from django.db import models
 
+
+#TODO do this from a conf file maybe even in the settings
 class SimulatorProfile(models.Model):
 
   name = models.CharField(max_length=50)
