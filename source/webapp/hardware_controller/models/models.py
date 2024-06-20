62087e8f3242b2439843dfd72bbf846736e07363
Paul Schillinger <uystq@student.kit.edu>
2024-06-20 22:21:23 +0200

    Hardwareprofil views

--- source/webapp/hardware_controller/models/models.py
+++ source/webapp/hardware_controller/models/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class Hardwareprofil(models.Model):
+    id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=200)
     ip = models.CharField(max_length=200)
--- source/webapp/hardware_controller/views.py
+++ source/webapp/hardware_controller/views.py
@@ -1,1 +1,38 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+from .models import Hardwareprofil
+
+
+def index(request):
+    hardwareprofiles = Hardwareprofil.objects.all()
+    template = loader.get_template("polls/index.html")
+    context = {
+        "hardwareprofiles": hardwareprofiles,
+    }
+    return HttpResponse(template.render(context, request))
+
+def add_hardwareprofile(request):
+    description = request.POST["description"]
+    ip = request.POST["ip"]
+    hardwareprofile = Hardwareprofil(description=description, ip=ip)
+    hardwareprofile.save()
+    return HttpResponse("Hardwareprofile added")
+
+def delete_hardwareprofile(request):
+    hardwareprofile_id = request.POST["hardwareprofile_id"]
+    hardwareprofile = Hardwareprofil.objects.get(pk=hardwareprofile_id)
+    hardwareprofile.delete()
+    return HttpResponse("Hardwareprofile deleted")
+
+def update_hardwareprofile(request):
+    hardwareprofile_id = request.POST["hardwareprofile_id"]
+    description = request.POST["description"]
+    ip = request.POST["ip"]
+    hardwareprofile = Hardwareprofil.objects.get(pk=hardwareprofile_id)
+    hardwareprofile.description = description
+    hardwareprofile.ip = ip
+    hardwareprofile.save()
+    return HttpResponse("Hardwareprofile updated")
