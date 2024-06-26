c7a3ea913895c6f25136e272147ec797f0872e17
Julius Haag <haag.julius@outlook.de>
2024-06-26 14:14:53 +0200

    Urls config for routing

--- quafelweb/simulation_view/urls.py
+++ quafelweb/simulation_view/urls.py
@@ -1,10 +1,9 @@
 
 from django.urls import include, path
-from django.views.generic.base import RedirectView
+
 from simulation_view.views import SimulationView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='view', permanent=False), name='index'),
-    path('view/', SimulationView.index, name='view'),
-    path('view/<int:qbit_start>:<int:qbit_end>:<int:depth_start>:<int:depth_end>:<int:evals_start>:<int:evals_end>/', SimulationView.specific, name='view')
+    path('', SimulationView.index, name='view'),
+    path('<int:qbit_start>:<int:qbit_end>:<int:depth_start>:<int:depth_end>:<int:evals_start>:<int:evals_end>/', SimulationView.specific, name='view')
 ]
