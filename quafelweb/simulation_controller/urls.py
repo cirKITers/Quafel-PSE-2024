8e4eb06c5580aef18298f84ecf2fcfa6e1132698
Julius Haag <haag.julius@outlook.de>
2024-08-23 13:14:32 +0200

    Seperation of conf and commit page

--- quafelweb/simulation_controller/urls.py
+++ quafelweb/simulation_controller/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 import simulation_controller.views as views
 
 urlpatterns = [
-    path("", views.SimulationRequestView.selection, name="simulation_configuration"),
+    path("", views.SimulationRequestView.configuration, name="simulation_configuration"),
+    path("confirm/", views.SimulationRequestView.confirmation, name="simulation_confirmation"),
     path("submit/", views.SimulationRequestView.submit_request, name="submit"),
     path(
         "submit_request/",
